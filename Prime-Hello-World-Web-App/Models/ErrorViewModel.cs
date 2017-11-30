3255536b840d6415f4df641932782962b08da33c
Eric Friday <ericfriday@gmail.com>
2017-11-29 18:33:46 -0600

    first commit

---
+++ Prime-Hello-World-Web-App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Prime_Hello_World_Web_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
